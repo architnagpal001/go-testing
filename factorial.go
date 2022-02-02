220ebe043e4489964fde8bea51f38fc036f59700
Archit Nagpal <architnagpal312@gmail.com>
2022-02-02 23:23:58 +0530

    test 2

--- factorial.go
+++ factorial.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+// import "fmt"
+
+func Factorial(input int) int{
+	fact := 1
+	for i:=input ; i>0 ; i-- {
+		fact *= 1
+	}
+	return fact
+}
 
 func main() {
-	fmt.Println("The Factorial is something!")
+	// fmt.Println("The Factorial is something!")
 }
