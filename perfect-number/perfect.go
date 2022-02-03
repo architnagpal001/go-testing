1b945e5daf5eca3cacff20fcb6e3289a962a226c
Archit Nagpal <architnagpal312@gmail.com>
2022-02-03 23:10:55 +0530

    test 4

--- perfect-number/perfect.go
+++ perfect-number/perfect.go
@@ -1,21 +1,24 @@
 package main
+
 import "fmt"
 
 func main() {
- //Enter your code here. Read input from STDIN. Print output to STDOUT
-    var num int
-    sum := 0
-    fmt.Println("Enter a number :")
-    fmt.Scanln(&num)
-    
-    for i:=1 ; i < num ; {
-        if num%i==0 {
-            sum = sum + 1
-            i++
-        }
-        if sum==num{
-            return 1
-        }
-        return 0
-    }
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	var num int
+	sum := 0
+	fmt.Println("Enter a number :")
+	fmt.Scanln(&num)
+
+	for i := 1; i < num; i++ {
+		if num%i == 0 {
+			sum = sum + i
+		}
+	}
+	if sum==num {
+		fmt.Println("1")
+	} else {
+		fmt.Println("0")
+	}
+
 }
+// 1,2,4,8,16,32
